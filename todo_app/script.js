91d7b3c3746365d9a9288261a72f2e6b8ef50323
mumtazkakakhail <mumtazkakakhail@gmail.com>
2025-03-16 20:50:37 +0500

    Real time js app

--- todo_app/script.js
+++ todo_app/script.js
@@ -1,43 +1,40 @@
 function addTodoList() {
-    const inputValue = document.getElementById("todoInput");
-    const textValue = inputValue.value;
-    const todoText = document.getElementById("todoData");
-    const errorMsg = document.getElementById("errorMsg");
-  
-    if (textValue === "") {
-      errorMsg.textContent = "please add todo";
-      return;
-    }
-  
-    errorMsg.textContent = "";
-    //   li create
-    const li = document.createElement("li");
-    li.textContent = textValue;
-    todoText.append(li);
-    //   console.log(li);
-  
-    const dltBtn = document.createElement("button");
-    dltBtn.textContent = "delete";
-    console.log(dltBtn);
-    dltBtn.className = "delete_btn";
-    dltBtn.onclick = function () {
-      console.log("delete");
-      todoText.removeChild(li);
-    };
-  
-    li.appendChild(dltBtn);
+  const inputValue = document.getElementById("todoInput");
+  const textValue = inputValue.value;
+  const todoText = document.getElementById("todoData");
+  const errorMsg = document.getElementById("errorMsg");
 
-   }
+  if (textValue === "") {
+    errorMsg.textContent = "please add todo";
+    return;
+  }
 
-  const editBtn = document.createElement("edit");
-  dltBtn.textContent = "edit";
-  console.log( editBtn);
-  editBtn.className = "edit_btn";
-  editBtn.onclick = function () {
-    console.log("edit");
+  errorMsg.textContent = "";
+  //   li create
+  const li = document.createElement("li");
+  li.textContent = textValue;
+  todoText.append(li);
+  //   console.log(li);
+
+  const dltBtn = document.createElement("button");
+  dltBtn.textContent = "delete";
+  console.log(dltBtn);
+  dltBtn.className = "delete_btn";
+  dltBtn.onclick = function () {
+    console.log("delete");
     todoText.removeChild(li);
   };
 
-  li.appendChild( editBtn);
- 
-  
+  li.appendChild(dltBtn);
+}
+
+const editBtn = document.createElement("edit");
+dltBtn.textContent = "edit";
+console.log(editBtn);
+editBtn.className = "edit_btn";
+editBtn.onclick = function () {
+  console.log("edit");
+  todoText.removeChild(li);
+};
+
+li.appendChild(editBtn);
